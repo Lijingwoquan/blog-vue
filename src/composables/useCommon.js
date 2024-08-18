83139d476de9d94cc2aff14d1cb2e9631efebfb3
lzh <2115883273@qq.com>
2024-08-18 20:29:54 +0800

    useCommon重构

--- src/composables/useCommon.js
+++ src/composables/useCommon.js
@@ -2,12 +2,19 @@ import { reactive, ref } from "vue";
 import { setIndexPage, getIndexPage } from "~/composables/auth.js";
 import { showLoading } from "~/composables/util.js";
 
-export function useCommonInitData(opt = {}) {
+// 获取数据 分页 loading状态
+export function useCommonGetData(opt = {}) {
   const searchForm = reactive({ ...opt.form });
+  const id = ref(opt.id);
   const tableData = ref([]);
+  const OneData = ref({});
   const currentPage = ref(1);
   const totalPages = ref(0);
   const loading = ref(false);
+
+  if (!opt.loadingText) {
+    opt.loadingText = "正在加载";
+  }
 
   const getOldPage = () => {
     if (searchForm.page) {
@@ -20,19 +27,31 @@ export function useCommonInitData(opt = {}) {
       searchForm.page = currentPage.value;
     }
   };
-
   getOldPage();
 
-  const getData = async () => {
+  const getOneData = async () => {
+    loading.value = false;
+    await showLoading(opt.loadingText);
+    await opt
+      .getOneData(id.value)
+      .then((res) => {
+        OneData.value = res;
+      })
+      .finally(() => {
+        loading.value = true;
+      });
+  };
+
+  const getDataList = async () => {
     if (searchForm.page) {
       searchForm.page = currentPage.value;
     }
-    if (typeof opt.getData === "function") {
+    if (typeof opt.getDataList === "function") {
       loading.value = true;
       tableData.value = [];
-      await showLoading("文章列表渲染中");
-      opt
-        .getData(searchForm)
+      await showLoading(opt.loadingText);
+      await opt
+        .getDataList(searchForm)
         .then((res) => {
           tableData.value = res.list;
           totalPages.value = res.totalPages;
@@ -47,22 +66,55 @@ export function useCommonInitData(opt = {}) {
         });
     }
   };
-  getData();
 
   return {
     searchForm,
+    id,
     tableData,
+    OneData,
     currentPage,
     totalPages,
     loading,
-    getData,
+    getOneData,
+    getDataList,
   };
 }
 
-export function useCommonInitForm(opt = {}) {
+// 增删改 简而言之就是除get以外的请求
+export function useCommonForm(opt = {}) {
   let form = opt.form;
+
+  const handleUpdate = () => {
+    loading.value = true;
+    if (typeof opt.update === "function") {
+      opt
+        .update(form)
+        .then(() => {
+          toast("更新成功");
+        })
+        .finally(() => {
+          loading.value = false;
+        });
+    }
+  };
+
+  const handelDelete = () => {
+    loading.value = true;
+    if (typeof opt.delete === "function")
+      opt
+        .delete(form)
+        .then(() => {
+          toast("删除成功");
+        })
+        .finally(() => {
+          loading.value = false;
+        });
+  };
+
   return {
     form,
+    handleUpdate,
+    handelDelete,
   };
 }
 
@@ -79,7 +131,7 @@ export function useCommonNav(opt = {}) {
     if (opt.currentPage) {
       opt.currentPage.value = p;
       setIndexPage(p);
-      opt.getData();
+      opt.getDataList();
     }
   };
 
