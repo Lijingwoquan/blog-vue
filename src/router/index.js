2204708d803d6c60fd440621fde0bc8be2fed9fd
lzh <2115883273@qq.com>
2024-09-30 13:33:48 +0800

    路由分组

--- src/router/index.js
+++ src/router/index.js
@@ -1,81 +1,40 @@
 import { createRouter, createWebHistory } from "vue-router";
-
 import { config } from "/config.js";
 
-const Login = () => import("~/pages/login.vue");
-const blog = () => import("~/layout/blog.vue");
-const blogAdmin = () => import("~/layout/blogAdmin.vue");
+// 使用命名的动态导入
+const Login = () => import(/* webpackChunkName: "login" */ "~/pages/login.vue");
+const Blog = () => import(/* webpackChunkName: "blog" */ "~/layout/blog.vue");
+const BlogAdmin = () =>
+  import(/* webpackChunkName: "blogAdmin" */ "~/layout/blogAdmin.vue");
+const NotFound = () =>
+  import(/* webpackChunkName: "notFound" */ "~/pages/404.vue");
 
-const userIndex = () => import("~/pages/userIndex.vue");
-const userClassify = () => import("~/pages/userClassify.vue");
-const userEssay = () => import("~/pages/userEssay/list.vue");
-
-const adminIndex = () => import("~/pages/adminIndex.vue");
-const adminCreateEssay = () => import("~/pages/adminCreateEssay.vue");
-const adminManageEssay = () => import("~/pages/adminManageEssay.vue");
-const adminUpdateData = () => import("~/pages/adminUpdateData/list.vue");
-const NotFound = () => import("~/pages/404.vue");
-
-//默认路由 所有用户共享
-const routes = [
-  {
-    path: "/login",
-    component: Login,
-  },
-  {
-    path: "/",
-    component: blog,
-    name: "blog",
-  },
-  {
-    path: `${config.MANAGER_URL}`,
-    component: blogAdmin,
-    name: "blogAdmin",
-  },
-  {
-    path: "/:pathMatch(.*)*",
-    name: "NotFound",
-    component: NotFound,
-  },
-];
-
-const userIndexRouter = {
-  path: "/",
-  component: userIndex,
-  meta: {
-    title: "首页",
-  },
+// 将用户相关页面分组
+const UserPages = {
+  Index: () => import(/* webpackChunkName: "user" */ "~/pages/userIndex.vue"),
+  Classify: () =>
+    import(/* webpackChunkName: "user" */ "~/pages/userClassify.vue"),
+  Essay: () =>
+    import(/* webpackChunkName: "user" */ "~/pages/userEssay/list.vue"),
 };
 
-const adminRouters = [
-  {
-    path: `${config.MANAGER_URL}/`,
-    component: adminIndex,
-    meta: {
-      title: "后台首页",
-    },
-  },
-  {
-    path: `${config.MANAGER_URL}/createEssay`,
-    component: adminCreateEssay,
-    meta: {
-      title: "新建文章",
-    },
-  },
-  {
-    path: `${config.MANAGER_URL}/manageEssay`,
-    component: adminManageEssay,
-    meta: {
-      title: "管理文章",
-    },
-  },
-  {
-    path: `${config.MANAGER_URL}/updateData`,
-    component: adminUpdateData,
-    meta: {
-      title: "更新数据",
-    },
-  },
+// 将管理员相关页面分组
+const AdminPages = {
+  Index: () => import(/* webpackChunkName: "admin" */ "~/pages/adminIndex.vue"),
+  CreateEssay: () =>
+    import(/* webpackChunkName: "admin" */ "~/pages/adminCreateEssay.vue"),
+  ManageEssay: () =>
+    import(/* webpackChunkName: "admin" */ "~/pages/adminManageEssay.vue"),
+  UpdateData: () =>
+    import(/* webpackChunkName: "admin" */ "~/pages/adminUpdateData/list.vue"),
+};
+
+// 默认路由 所有用户共享
+const routes = [
+  { path: "/login", component: Login },
+  { path: "/", component: Blog, name: "blog" },
+  { path: `${config.MANAGER_URL}`, component: BlogAdmin, name: "blogAdmin" },
+  { path: "/:pathMatch(.*)*", name: "NotFound", component: NotFound },
 ];
 
 export const router = createRouter({
@@ -83,68 +42,79 @@ export const router = createRouter({
   routes,
 });
 
-//添加首页路由
+// 添加首页路由
 export function addUserIndexRouter() {
-  let hasNewRoutes = false;
-  if (!router.hasRoute(userIndexRouter.path)) {
-    hasNewRoutes = true;
-    router.addRoute("blog", userIndexRouter);
+  if (!router.hasRoute("/")) {
+    router.addRoute("blog", {
+      path: "/",
+      component: UserPages.Index,
+      meta: { title: "首页" },
+    });
+    return true;
   }
-  return hasNewRoutes;
+  return false;
 }
 
-//添加管理路由
+// 添加管理路由
 export function addAdminIndexRouter() {
-  adminRouters.forEach((component) => {
-    if (!router.hasRoute(component.path)) {
-      router.addRoute("blogAdmin", component);
-      // hasNewRoutes = true;
+  const adminRoutes = [
+    {
+      path: `${config.MANAGER_URL}/`,
+      component: AdminPages.Index,
+      meta: { title: "后台首页" },
+    },
+    {
+      path: `${config.MANAGER_URL}/createEssay`,
+      component: AdminPages.CreateEssay,
+      meta: { title: "新建文章" },
+    },
+    {
+      path: `${config.MANAGER_URL}/manageEssay`,
+      component: AdminPages.ManageEssay,
+      meta: { title: "管理文章" },
+    },
+    {
+      path: `${config.MANAGER_URL}/updateData`,
+      component: AdminPages.UpdateData,
+      meta: { title: "更新数据" },
+    },
+  ];
+
+  adminRoutes.forEach((route) => {
+    if (!router.hasRoute(route.path)) {
+      router.addRoute("blogAdmin", route);
     }
   });
 }
 
-//动态添加路由
+// 动态添加路由
 export function addIndexMenuRouters(menu) {
-  //是否有新的路由
   let hasNewRoutes = false;
-  const findAndAddRoutes = (menu) => {
-    menu.forEach((o) => {
-      let classifyList = o.classifyList;
-      classifyList.forEach((classifyObj) => {
-        let classify = {
-          path: decodeURIComponent("/classify" + classifyObj.router),
-          component: userClassify,
-          meta: {
-            title: classifyObj.name,
-          },
-        };
-        if (!router.hasRoute(classify.path)) {
-          hasNewRoutes = true;
-          router.addRoute("blog", classify);
-        }
-      });
+  menu.forEach((o) => {
+    o.classifyList.forEach((classifyObj) => {
+      const path = decodeURIComponent("/classify" + classifyObj.router);
+      if (!router.hasRoute(path)) {
+        router.addRoute("blog", {
+          path,
+          component: UserPages.Classify,
+          meta: { title: classifyObj.name },
+        });
+        hasNewRoutes = true;
+      }
     });
-  };
-
-  findAndAddRoutes(menu);
-
+  });
   return hasNewRoutes;
 }
 
 export function addEssayRouters(essayRoute) {
-  let hasNewRoutes = false;
-  let essay = {
-    path: essayRoute,
-    component: userEssay,
-    meta: {
-      title: essayRoute,
-    },
-    name: essayRoute,
-  };
-
   if (!router.hasRoute(essayRoute)) {
-    hasNewRoutes = true;
-    router.addRoute("blog", essay);
+    router.addRoute("blog", {
+      path: essayRoute,
+      component: UserPages.Essay,
+      meta: { title: essayRoute },
+      name: essayRoute,
+    });
+    return true;
   }
-  return hasNewRoutes;
+  return false;
 }
